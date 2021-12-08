5b3cd34f3956b39283b146efd1ef921de66d1412
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-12-08 20:55:43 +0300

    feat: add example page

---
+++ example/createSliderControls.js
@@ -0,0 +1,99 @@
+import createElement from '@/helpers/createElement';
+
+function createInputs({ container, items, type }) {
+  const inputsContainer = createElement('div', {
+    class: 'slider-controls__inputs-container',
+  });
+
+  items.forEach(({ name, handler, getValue }) => {
+    const inputClasses = `slider-controls__input ${
+      type === 'checkbox' ? 'slider-controls__input--checkbox' : ''
+    }`;
+
+    const input = createElement('input', {
+      class: inputClasses,
+      type: type,
+      name: name,
+      ...(type === 'checkbox' && { checked: getValue() }),
+      ...(type === 'number' && { value: getValue() }),
+    });
+
+    const labelText = createElement(
+      'span',
+      {
+        class: 'slider-controls__input-label-text',
+      },
+      [name],
+    );
+
+    const label = createElement(
+      'label',
+      {
+        class: 'slider-controls__input-label',
+      },
+      [labelText, input],
+    );
+
+    const inputContainer = createElement(
+      'div',
+      {
+        class: 'slider-controls__input-container',
+      },
+      [label],
+    );
+
+    const handleInputChange = ({ target }) => {
+      if (target.type === 'checkbox') handler(target.checked);
+      if (target.type === 'number') handler(target.value);
+    };
+
+    inputsContainer.addEventListener('change', handleInputChange);
+
+    inputsContainer.append(inputContainer);
+  });
+
+  container.append(inputsContainer);
+}
+
+function createSliderControls({ container, $instance }) {
+  const numberInputs = [
+    // { name: 'min', handler: $instance.setMin, getValue: $instance.getMin },
+    // { name: 'max', handler: $instance.setMax, getValue: $instance.getMax },
+    // { name: 'step', handler: $instance.setStep, getValue: $instance.getStep },
+    { name: 'from', handler: $instance.setFrom, getValue: $instance.getFrom },
+    // { name: 'to', handler: $instance.setTo, getValue: $instance.getTo },
+  ];
+
+  const booleanInputs = [
+    // {
+    //   name: 'vertical',
+    //   handler: $instance.setIsVertical,
+    //   getValue: $instance.getIsVertical,
+    // },
+    // {
+    //   name: 'range',
+    //   handler: $instance.setIsRange,
+    //   getValue: $instance.getIsRange,
+    // },
+    // {
+    //   name: 'scale',
+    //   handler: $instance.showScale,
+    //   getValue: $instance.getShowScale,
+    // },
+    // { name: 'bar', handler: $instance.showBar, getValue: $instance.getShowBar },
+    // { name: 'tip', handler: $instance.showTip, getValue: $instance.getShowTip },
+  ];
+
+  createInputs({
+    container,
+    items: numberInputs,
+    type: 'number',
+  });
+  createInputs({
+    container,
+    items: booleanInputs,
+    type: 'checkbox',
+  });
+}
+
+export default createSliderControls;
---
+++ example/example.js
@@ -0,0 +1,104 @@
+import './styles/example.scss';
+
+import * as $ from 'jquery';
+import '../src/ts/slider';
+
+import createElement from '@/helpers/createElement';
+import createSliderControls from './createSliderControls';
+
+function createSliderExample({ heading, properties }) {
+  const sliderContainer = createElement('div', {
+    class: 'slider-example__slider-container',
+  });
+  const sliderControlsContainer = createElement('div', {
+    class: 'slider-example__controls-container',
+  });
+  const sliderSectionHeading = createElement(
+    'div',
+    { class: 'slider-example__heading' },
+    [heading],
+  );
+  const sliderWrapper = createElement(
+    'div',
+    { class: 'slider-example__wrapper' },
+    [sliderSectionHeading, sliderContainer, sliderControlsContainer],
+  );
+
+  document.body.append(sliderWrapper);
+
+  const $slider = $(sliderContainer).createSlider(properties);
+  createSliderControls({
+    container: sliderControlsContainer,
+    $instance: $slider,
+  });
+
+  return $slider;
+}
+
+const firstSliderProperties = {
+  from: 0,
+  min: -50,
+  max: 50,
+  step: 10,
+  showBar: true,
+  showScale: true,
+  showTip: true,
+  isRange: false,
+  isVertical: false,
+};
+
+const $firstSlider = createSliderExample({
+  heading: 'First slider',
+  properties: firstSliderProperties,
+});
+
+const secondSliderProperties = {
+  from: 0,
+  min: -50,
+  max: 50,
+  step: 10,
+  showBar: true,
+  showScale: true,
+  showTip: true,
+  isRange: true,
+  isVertical: true,
+};
+
+const $secondSlider = createSliderExample({
+  heading: 'Second slider',
+  properties: secondSliderProperties,
+});
+
+const thirdSliderProperties = {
+  from: 0,
+  min: -50,
+  max: 50,
+  step: 10,
+  showBar: true,
+  showScale: true,
+  showTip: true,
+  isRange: true,
+  isVertical: true,
+};
+
+const $thirdSlider = createSliderExample({
+  heading: 'Third slider',
+  properties: thirdSliderProperties,
+});
+
+const fourthSliderProperties = {
+  from: 0,
+  min: -50,
+  max: 50,
+  step: 10,
+  showBar: true,
+  showScale: true,
+  showTip: true,
+  isRange: true,
+  isVertical: true,
+};
+
+const $fourthSlider = createSliderExample({
+  heading: 'Fourth slider',
+  properties: fourthSliderProperties,
+});
