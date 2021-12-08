64676fb08919464f05dbe30e9237c381921989b6
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-12-08 20:18:25 +0300

    feat: add ability to set attributes and child elements to create element function

--- src/helpers/createElement.ts
+++ src/helpers/createElement.ts
@@ -1,6 +1,19 @@
-function createElement(tag: string, className?: string) {
+function createElement(
+  tag: string,
+  attributes: string[],
+  children?: HTMLElement[],
+) {
   const element = document.createElement(tag);
-  if (className) element.classList.add(className);
+
+  Object.entries(attributes).forEach(([attribute, value]: [string, string]) => {
+    element.setAttribute(attribute, value);
+  });
+
+  if (children) {
+    children?.forEach((child) => {
+      element.append(child);
+    });
+  }
 
   return element;
 }
