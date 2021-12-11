3d4e62b1e61d5834b7716a049c13fd81424ae926
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-12-11 22:11:56 +0300

    refactor: fix types and update render of bar

--- src/views/Bar.ts
+++ src/views/Bar.ts
@@ -1,22 +1,26 @@
-import createElement from '../helpers/createElement';
+import createElement from '@/helpers/createElement';
 
 class Bar {
-  element: HTMLElement;
+  element: HTMLElement | null;
 
   constructor() {
-    this.element;
+    this.element = null;
   }
 
   render(parent: HTMLElement) {
-    this.element = createElement('div', 'slider-bar');
+    this.element = createElement('div', { class: 'slider-bar' });
     parent.append(this.element);
   }
 
   update(firstThumbOffset: number) {
+    if (!this.element) return;
+
     this.element.style.width = `${String(firstThumbOffset)}px`;
   }
 
   updateRange(firstThumbOffset: number, secondThumbOffset: number) {
+    if (!this.element) return;
+
     const distanceBetween = secondThumbOffset - firstThumbOffset;
 
     this.element.style.left = `${String(firstThumbOffset)}px`;
