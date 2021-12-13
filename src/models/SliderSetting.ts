36f11a7c8245bfadaf4db82fc2660ef4442a474c
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-12-13 19:55:00 +0300

    feat: update slider setting type with new values

--- src/models/SliderSetting.ts
+++ src/models/SliderSetting.ts
@@ -1,9 +1,13 @@
 interface SliderSettings {
-  from?: number;
+  fromIndex: number;
+  minIndex: number;
   min?: number;
+  maxIndex: number;
   max?: number;
-  step?: number;
-  to?: number;
+  stepIndex?: number;
+  toIndex?: number;
+  values?: number[] | string[];
+  generatorFn?: () => number[] | string[];
   showBar?: boolean;
   showScale?: boolean;
   showTip?: boolean;
