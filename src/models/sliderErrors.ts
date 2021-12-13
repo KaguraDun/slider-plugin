4c77f21b0569067179cd0fe9b1d9c23dbf56145d
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-12-13 20:01:12 +0300

    feat: add option out of range error

--- src/models/sliderErrors.ts
+++ src/models/sliderErrors.ts
@@ -5,6 +5,9 @@ const sliderErrors = {
   throwOptionNotValid: (option: string) => {
     console.log(`${option} isn't a valid option`);
   },
+  throwOptionOutOfRange: (option: string, min: string, max: string) => {
+    console.log(`${option} option out of range [${min}:${max}]`);
+  },
 };
 
 export default sliderErrors;
