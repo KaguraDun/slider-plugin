d01cc29a8b9b54327f70dedad2d74d1c17984884
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-12-15 15:04:01 +0300

    refactor: tip
    refactor: upate tip instead rerender
    feat: add show method to hide tip conditionally
    fix: wrong types;

--- src/views/Tip.ts
+++ src/views/Tip.ts
@@ -1,23 +1,41 @@
 import createElement from '@/helpers/createElement';
 
+interface TipProps {
+  parent: HTMLElement;
+  value: number | string;
+  isVertical: boolean;
+  showTip: boolean;
+}
+
 class Tip {
-  element: HTMLElement | null;
+  element: HTMLElement;
+  parent: HTMLElement | null;
 
   constructor() {
-    this.element = null;
+    this.parent = null;
+    this.element = createElement('div', { class: 'slider__tip' });
   }
 
-  render(parent: HTMLElement, value: number, isVertical: boolean) {
-    this.element = createElement('div', { class: 'slider-tip' });
+  render({ parent, value, isVertical, showTip }: TipProps) {
+    this.parent = parent;
     this.element.innerText = String(value);
-    if (isVertical) this.element.classList.add('slider-tip--vertical');
-    parent.append(this.element);
+
+    if (isVertical) this.element.classList.add('slider__tip--vertical');
+    this.show(showTip);
   }
 
-  setValue(value: number) {
+  update(value: number | string) {
     if (!this.element) return;
 
     this.element.innerText = String(value);
+  }
+
+  show(showTip: boolean) {
+    if (showTip && this.parent) {
+      this.parent.append(this.element);
+    } else {
+      this.element.remove();
+    }
   }
 }
 
