171c66268f53cfcc55cbb33a37b40fb51ba28221
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-12-15 16:37:32 +0300

    feat: update scale
    refactor: rerender only scale marks
    feat: limit number of scale marks
    feat: add ability to set from value by mark click
    fix: scale types;

--- src/views/Scale.ts
+++ src/views/Scale.ts
@@ -1,23 +1,74 @@
 import createElement from '@/helpers/createElement';
-class Scale {
-  element: HTMLElement | null;
+import SliderSettings from '@/models/SliderSetting';
+import { Subject } from '@/observer/Observer';
 
-  constructor() {
-    this.element = null;
+class Scale {
+  parent: HTMLElement | null;
+  scaleClickEvent: Subject;
+  element: HTMLElement;
+
+  constructor(scaleClick: Subject) {
+    this.parent = null;
+    this.scaleClickEvent = scaleClick;
+    this.element = createElement('div', {
+      class: 'slider__scale',
+    });
+
+    this.handleScaleClick = this.handleScaleClick.bind(this);
   }
 
-  render(parent: HTMLElement, values: []) {
-    this.element = createElement('div', { class: 'slider-scale' });
+  handleScaleClick(clickEvent: MouseEvent) {
+    const target = <HTMLElement>clickEvent.target;
+    const closest = target.closest('.slider__scale-mark');
 
-    values.forEach((item) => {
-      const mark = createElement('div', { class: 'slider-scale-mark' });
-      mark.innerText = item;
+    if (!closest) return;
+
+    const ID = target.dataset.id;
+
+    this.scaleClickEvent.notify({ fromIndex: ID });
+  }
+
+  render(parent: HTMLElement, state: SliderSettings) {
+    const { values, showScale } = state;
+
+    if (!values || !showScale) return;
+
+    const numberOfMiddleItems = 3;
+    const lastItem = 1;
+    const itemsStep = Math.round(
+      values?.length / numberOfMiddleItems - lastItem,
+    );
+
+    this.parent = parent;
+    this.element.innerHTML = '';
+
+    values?.forEach((item, index) => {
+      const isMiddleItem = index > 0 && index < values.length - 1;
+      const isFitToItemsStep = index % itemsStep === 0;
+
+      if (isMiddleItem) {
+        if (!isFitToItemsStep) return;
+      }
+
+      const mark = createElement(
+        'span',
+        { class: 'slider__scale-mark', ['data-id']: String(index) },
+        [String(item)],
+      );
       this.element?.append(mark);
     });
 
     // if (isVertical) this.element.classList.add('slider-scale--vertical');
+    this.show(showScale);
+  }
 
-    parent.append(this.element);
+  show(showScale: boolean) {
+    if (showScale) {
+      this.parent?.append(this.element);
+      this.element.addEventListener('click', this.handleScaleClick);
+    } else {
+      this.element.remove();
+    }
   }
 }
 
