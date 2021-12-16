8f68f30eaa2552e17737ebaa94a7d4d9ea8aecad
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-12-16 19:57:48 +0300

    fix: missing scale click observer event

--- src/observer/ObserverEvents.ts
+++ src/observer/ObserverEvents.ts
@@ -3,10 +3,12 @@ import { Subject } from './Observer';
 class ObserverEvents {
   stateChanged: Subject;
   thumbMoved: Subject;
+  scaleClick: Subject;
 
   constructor() {
     this.stateChanged = new Subject();
     this.thumbMoved = new Subject();
+    this.scaleClick = new Subject();
   }
 }
 
