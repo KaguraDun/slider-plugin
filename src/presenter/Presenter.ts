ba75682eb94bcf3329b23617a85aa8c2d88244d7
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2021-12-18 18:47:36 +0300

    feat: add all slider options in presenter
    refactor: rename methods
    refactor: create slider function according to the new architecture

--- src/presenter/Presenter.ts
+++ src/presenter/Presenter.ts
@@ -18,59 +18,107 @@ class Presenter {
   }
 
   createSlider(container: HTMLElement, options?: SliderSettings) {
+    const { from, to, ...restOptions } = options;
+
     if (!container) {
       sliderErrors.throwContainerNotFound();
       return;
     }
 
-    this.view.init(container);
-    this.observerEvents.thumbMoved.attach(this.model.setOptions);
-
     if (options) {
-      this.model.setOptions(options);
+      this.model.setOptions(restOptions);
     } else {
       this.model.setDefaultSettings();
     }
-  }
 
-  getFrom() {
-    return this.model.getFrom();
+    this.model.setFrom(from);
+    if (to) this.model.setTo(to);
+
+    this.view.init(container, this.model.getState());
+
+    this.observerEvents.stateChanged.attach(this.view.update);
+    this.observerEvents.thumbMoved.attach(this.model.setOptions);
+    this.observerEvents.scaleClick.attach(this.model.setOptions);
   }
 
   setFrom(from: number) {
     this.model.setFrom(from);
   }
 
-  showScale(show: boolean) {
+  getFrom() {
+    return this.model.getFrom();
+  }
+
+  setTo(to: number | string) {
+    this.model.setTo(to);
+  }
+
+  getTo() {
+    return this.model.getTo();
+  }
+
+  setShowScale(show: boolean) {
     this.model.setShowScale(show);
   }
 
-  showTip(show: boolean) {
+  getShowScale() {
+    return this.model.getShowScale();
+  }
+
+  setShowTip(show: boolean) {
     this.model.setShowTip(show);
   }
 
-  showBar(show: boolean) {
+  getShowTip() {
+    return this.model.getShowTip();
+  }
+
+  setShowBar(show: boolean) {
     this.model.setShowBar(show);
   }
 
-  setRange(isRange: boolean) {
+  getShowBar() {
+    return this.model.getShowBar();
+  }
+
+  setIsRange(isRange: boolean) {
     this.model.setIsRange(isRange);
   }
 
-  setVertical(isVertical: boolean) {
+  getIsRange() {
+    return this.model.getIsRange();
+  }
+
+  setIsVertical(isVertical: boolean) {
     this.model.setIsVertical(isVertical);
+  }
+
+  getIsVertical() {
+    return this.model.getIsVertical();
   }
 
   setMin(min: number) {
     this.model.setMin(min);
   }
 
+  getMin() {
+    return this.model.getMin();
+  }
+
   setMax(max: number) {
     this.model.setMax(max);
   }
 
+  getMax() {
+    return this.model.getMax();
+  }
+
   setStep(step: number) {
     this.model.setStep(step);
+  }
+
+  getStep() {
+    return this.model.getStep();
   }
 }
 
