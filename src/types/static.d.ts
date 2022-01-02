f4c693ed25b4198b9ab2fd2a5cf7872b911a40cd
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2022-01-02 16:23:49 +0300

    fix: types of slider plugin

---
+++ src/types/static.d.ts
@@ -0,0 +1,32 @@
+import SliderSettings from '@/models/SliderSetting';
+
+interface SliderMethods {
+  fromChangedEvent(callback: () => void): SliderMethods;
+  toChangedEvent(callback: () => void): SliderMethods;
+  setFrom(from: number): SliderMethods;
+  getFrom(): number;
+  setTo(to: number): SliderMethods;
+  getTo(): number;
+  setStep(step: number): SliderMethods;
+  getStep(): number;
+  setMin(min: number): SliderMethods;
+  getMin(): number;
+  setMax(max: number): SliderMethods;
+  getMax(): number;
+  setShowTip(show: boolean): SliderMethods;
+  getShowTip(): boolean;
+  setShowScale(show: boolean): SliderMethods;
+  getShowScale(): boolean;
+  setShowBar(show: boolean): SliderMethods;
+  getShowBar(): boolean;
+  setIsRange(isRange: boolean): SliderMethods;
+  getIsRange(): boolean;
+  setIsVertical(isVertical: boolean): SliderMethods;
+  getIsVertical(): boolean;
+}
+
+declare global {
+  interface JQuery {
+    createSlider: (options: SliderSettings) => SliderMethods;
+  }
+}
