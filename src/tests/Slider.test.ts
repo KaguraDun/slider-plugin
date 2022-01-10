7149872368a475613ebbe3bb98ca7b64a5a28847
Vasily Kovnev <54976219+KaguraDun@users.noreply.github.com>
2022-01-10 15:30:37 +0300

    test: add Slider tests

---
+++ src/tests/Slider.test.ts
@@ -0,0 +1,32 @@
+import Slider from '@/views/Slider';
+
+describe('Slider', () => {
+  const container = document.createElement('div');
+  document.body.append(container);
+
+  let slider: Slider | null = null;
+
+  beforeEach(() => {
+    slider = new Slider();
+  });
+
+  afterEach(() => {
+    container.innerHTML = '';
+  });
+
+  it('should render', () => {
+    slider!.render(container);
+
+    expect(slider!.element).toBeInTheDocument();
+  });
+
+  it('should toggle vertical', () => {
+    slider!.render(container);
+
+    slider!.toggleVertical(true);
+    expect(slider!.element).toHaveClass('slider_vertical');
+
+    slider!.toggleVertical(false);
+    expect(slider!.element).not.toHaveClass('slider_vertical');
+  });
+});
